76439b562f0b0f08bb7382bb4e597fdb2f5cb753
MertAKM <mertyagizakman5519@gmail.com>
2026-07-23 01:08:17 +0300

    Add basic 3D scene

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::*, window};
+use bevy::prelude::*;
 
 fn main(){
     App::new()
@@ -14,10 +14,39 @@ fn main(){
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn((
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(20.0, 20.0))),
+        MeshMaterial3d(materials.add(Color::srgb(0.2, 0.5, 0.2))),
+    ));
+    
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+        MeshMaterial3d(materials.add(Color::srgb(0.7, 0.2, 0.2))),
+    ));
+    
+    commands.spawn((
+        DirectionalLight {
+            shadow_maps_enabled: true,
+            illuminance: 10_000.0,
+            ..default()
+        },
+        Transform::from_rotation(Quat::from_euler(
+            EulerRot::XYZ,
+            -1.0,
+            -0.5,
+            0.0,
+        )),
+    ));
+
+
     commands.spawn((
         Camera3d::default(),
-        Transform::from_xyz(0.0, 2.0, 5.0)
+        Transform::from_xyz(4.0, 4.0, 6.0)
             .looking_at(Vec3::ZERO, Vec3::Y),  
     ));
     
