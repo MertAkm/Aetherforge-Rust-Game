c6a2a7f29a9b4cadcf7bafe01a595d4bec7d04de
MertAKM <mertyagizakman5519@gmail.com>
2026-07-23 00:16:58 +0300

    Create Bevy application window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use bevy::{prelude::*, window};
+
+fn main(){
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window{
+                title: "Atherforge".to_string(),
+                resolution: (1280, 720).into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .run();
 }
