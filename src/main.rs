811602521cf69a5a11bf6cfce956ec8f7fb001c2
MertAKM <mertyagizakman5519@gmail.com>
2026-07-23 00:31:11 +0300

    Add startup system and 3D camera

--- src/main.rs
+++ src/main.rs
@@ -10,5 +10,15 @@ fn main(){
             }),
             ..default()
         }))
+        .add_systems(Startup, setup)
         .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(0.0, 2.0, 5.0)
+            .looking_at(Vec3::ZERO, Vec3::Y),  
+    ));
+    
 }
