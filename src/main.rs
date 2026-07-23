2f02f85a684b00f2a1e0db5e6b10047f2f4eab0d
MertAKM <mertyagizakman5519@gmail.com>
2026-07-23 14:31:03 +0300

    Add basic player movement

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,12 @@ fn main(){
             ..default()
         }))
         .add_systems(Startup, setup)
+        .add_systems(Update, player_movement)
         .run();
 }
+
+#[derive(Component)]
+struct Player;
 
 fn setup(
     mut commands: Commands,
@@ -42,12 +46,43 @@ fn setup(
             0.0,
         )),
     ));
-
-
+    
     commands.spawn((
         Camera3d::default(),
+        Player,
         Transform::from_xyz(4.0, 4.0, 6.0)
             .looking_at(Vec3::ZERO, Vec3::Y),  
     ));
+
+}
+
+fn player_movement(
+    kb_input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+    mut player_query: Query<&mut Transform, With<Player>>
+){
+    let Ok(mut transform) = player_query.single_mut() else {
+        return;
+    };
+
+    let mut direction = Vec3::ZERO;
+
+    if kb_input.pressed(KeyCode::KeyW) {
+        direction.z -= 1.0;
+    }
+    if kb_input.pressed(KeyCode::KeyA) {
+        direction.x -= 1.0;
+    }
+    if kb_input.pressed(KeyCode::KeyS) {
+        direction.z += 1.0;
+    }
+    if kb_input.pressed(KeyCode::KeyD) {
+        direction.x += 1.0;
+    }
+
+    let movement_speed = 5.0;
+
+    transform.translation +=
+        direction.normalize_or_zero() * movement_speed * time.delta_secs();
     
 }
